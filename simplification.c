df38211d233b20c26e087b68a8b84c5fbba09ddf
Edgar Onghena <46636609+edgarogh@users.noreply.github.com>
2021-02-19 12:11:06 +0100

    WartAdaptation

--- simplification.c
+++ simplification.c
@@ -3,37 +3,36 @@
 #include "simplification.h"
 
 
-ListePoints simplification_douglas_peucker(TableauPoints c, int i1, int i2, double seuil) {
-    double distance_max = 0;
-    int index_distance_max = i1;
-    for (int i = i1 + 1; i <= i2; i++) {
-        double distance = distance_point_segment(c.inner[i1], c.inner[i2], c.inner[i]);
-        if (distance_max < distance) {
-            distance_max = distance;
-            index_distance_max = i;
+typedef struct {
+    Point a, b;
+} Segment;
+
+
+ListePoints simplification_douglas_peucker_(TableauPoints T, int j1, int j2, double d){
+    double dmax = 0;
+    double dj;
+    int k = j1;
+    ListePoints Liste = liste_points_new();
+    ListePoints *L = &Liste;
+
+    Segment Pj1j2 = { T.inner[j1], T.inner[j2] };
+
+    for (int j=j1+1; j<=j2; j++){
+        dj = distance_point_segment(Pj1j2.a, Pj1j2.b, T.inner[j]);
+        if (dmax < dj){
+            dmax = dj;
+            k = j;
         }
     }
 
-    ListePoints L;
-    if (distance_max <= seuil) {
-        L = liste_points_new();
-        liste_points_push(&L, c.inner[i1]);
-        liste_points_push(&L, c.inner[i2]);
-    } else {
-        L = simplification_douglas_peucker(c, i1, index_distance_max, seuil);
-        ListePoints L2 = simplification_douglas_peucker(c, index_distance_max, i2, seuil);
-
-        // Le dernier élément de L est le même que le premier élément de L2, on enlève donc le 1er élément de L2
-        if (L2.first) {
-            ListePointsNoeud* first = L2.first;
-            ListePointsNoeud* second = first->next;
-            L2.first = second;
-            free(first);
-            L2.len--;
-        }
-
-        liste_points_concat(&L, L2);
+    if (dmax <= d){
+        liste_points_push(L, T.inner[j1]);
+        liste_points_push(L, T.inner[j2]);
+    }
+    else{
+        Liste = simplification_douglas_peucker(T, j1, k, d);
+        liste_points_concat(L, simplification_douglas_peucker(T, k, j2, d));
     }
 
-    return L;
+    return Liste;
 }
