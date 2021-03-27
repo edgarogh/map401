f15d8dca7d379cf05c7ee8a1d002a1c40a2bbae5
Edgar Onghena <dev@edgar.bzh>
2021-03-27 18:03:59 +0100

    Generify cr71.sh to Bezier3

--- cr7.sh
+++ cr7.sh
@@ -1,12 +1,24 @@
 #!/usr/bin/env bash
 
-# Génère les 15 images pour le CR de la tâche 7-1, en PNG, dimensions 400x400.
+# Génère les 15 images pour le CR de les tâches 7-*, en PNG, dimensions 400x400.
 # Il faut: `gs`, ImageMagick (pour le redimensionnement), `sponge` (paquet "moreutils" sur apt), clang + GNU make
 
 make contour_of
 
 files="Asterix3.pbm lettre-L-cursive.pbm Picasso-ColombesDeLaPaix.pbm"
-wd="cr71"
+
+if [ "$1" == "2" ]
+then
+  flag=b
+  wd=cr71
+elif [ "$1" == "3" ]
+then
+  flag=B
+  wd=cr72
+else
+  >&2 echo "utilisation: ./cr7 <2|3>"
+  exit 1
+fi
 
 mkdir -p $wd
 pushd $wd > /dev/null || exit
@@ -25,7 +37,7 @@ do
     then
       dd=""
     else
-      dd="-b $d"
+      dd="-$flag $d"
     fi
     >&2 ../contour_of $image $dd -3 -c
     gs -dSAFER -dBATCH -dNOPAUSE -dEPSCrop -r600 -sDEVICE=png16m -sOutputFile=$img $image"-mode3.eps" > /dev/null
